96ffa0df873ac53413a249528b738bae1d1cef9a
yuvaraj <yuvarajreddy24@gmail.com>
2025-10-23 21:44:20 +0530

    .

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+    backend "s3" {
+    bucket = "yuvarajreddy-1"
+    key    = "vpc-module-demo"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
