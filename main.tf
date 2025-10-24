d4b2599302ae25ba2c02d8bd1f57c55dc02ea5e5
yuvaraj <yuvarajreddy24@gmail.com>
2025-10-24 22:14:43 +0530

    .

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     # source = "../terraform-aws-vpc"
-    source = "git:: https://github.com/YuvarajReddy24/terraform-aws-vpc.git"
+    source = "git::https://github.com/YuvarajReddy24/terraform-aws-vpc.git?ref=main"
     # vpc_cidr = "10.0.0.0/16"
     # project_name = "roboshop"
     # environment = "dev"
